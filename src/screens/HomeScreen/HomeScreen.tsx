ff261df7f0bdebcc89dba4cffe6a39265026dcf3
Ky Nguyen <nguyentruongky33@gmail.com>
2021-01-09 02:54:43 +0700

    Init project with new name

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,23 +1,59 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import {Weight, getFont} from '@fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 export default function HomeScreen() {
   return (
-    <View style={{flex: 1, backgroundColor: 'rgb(258,86,63)'}}>
-      <Header />
+    <View style={{flex: 1, backgroundColor: 'rgb(258,86,63)', paddingTop: 16}}>
+      <QuoteCell />
+      <QuoteCell />
+      <QuoteCell />
+      <QuoteCell />
     </View>
   );
 }
 
-function Header() {
+function QuoteCell() {
+  const iconSize = 24;
   return (
-    <SafeAreaView style={{marginHorizontal: 16}}>
-      <Text style={{...getFont(Weight.regular, 12), color: 'white'}}>
-        What's good in
-      </Text>
-      <Text style={{...getFont(Weight.bold, 24), color: 'white'}}>
-        San Francisco
-      </Text>
+    <SafeAreaView
+      style={{
+        flexDirection: 'row',
+      }}>
+      <View
+        style={{
+          flex: 1,
+          marginTop: 16,
+          marginLeft: 16,
+          marginRight: 16,
+          marginBottom: 16,
+          padding: 16,
+          borderRadius: 16,
+          backgroundColor: '#000000AA',
+        }}>
+        <Text style={{...getFont(Weight.bold, 15), color: 'white'}}>
+          You’ve gotta dance like there’s nobody watching,Love like you’ll never
+          be hurt,Sing like there’s nobody listening,And live like it’s heaven
+          on earth.
+        </Text>
+        <Text
+          style={{
+            ...getFont(Weight.regular, 12),
+            color: '#ffffffAA',
+            textAlign: 'right',
+            marginTop: 8,
+          }}>
+          Don't make me cry
+        </Text>
+      </View>
+      <FontAwesome
+        name="bookmark"
+        size={30}
+        color="#900"
+        style={{marginRight: 16, marginTop: 16}}
+      />
     </SafeAreaView>
   );
 }
