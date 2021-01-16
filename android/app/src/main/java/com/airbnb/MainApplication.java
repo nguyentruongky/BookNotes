a5e8aae72a8c00e24bbb85fec42d15aab26f2b7f
Ky Nguyen <nguyentruongky33@gmail.com>
2021-01-16 23:17:06 +0700

    Change logo color

--- android/app/src/main/java/com/airbnb/MainApplication.java
+++ android/app/src/main/java/com/airbnb/MainApplication.java
@@ -7,8 +7,10 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -24,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
 				protected List<ReactPackage> getPackages() {
 					return Arrays.<ReactPackage>asList(
 							new MainReactPackage(),
-							new AsyncStoragePackage(),
+							new AsyncStoragePackage()
 					);
 				}
 
